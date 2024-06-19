40a52c988d9cacfff84f941baaf741d625fa10e3
CardialS <phugo.pereira14@gmail.com>
2024-06-19 15:57:13 +0200

    Updates made

---
+++ README.md
@@ -0,0 +1,70 @@
+# QueueManagement-main
+ Here are the changes made to the CSS to enhance the professional and aesthetic appearance of the navbar:
+
+Navbar Background Color:
+
+Changed the background color to #192839 (a dark blue) to give a more professional look.
+Logo Container:
+
+Set the width to auto to dynamically adjust based on the content.
+Ensured the elements are evenly spaced and aligned centrally.
+Logo Text:
+
+Changed the color to #FFFFFF (white) for high contrast against the dark background.
+Slightly reduced the font size to 1.6em for better proportion and balance.
+Maintained a font weight of 600 for emphasis and a professional appearance.
+Applied text-transform: uppercase to make the text uppercase for a more formal look.
+Added letter-spacing: 0.05em for improved readability.
+Ensured no default margin is applied by setting margin: 0.
+Navigation Menu:
+
+Set the width to auto to dynamically adjust based on the content.
+Aligned items centrally vertically for a uniform appearance.
+Used justify-content: space-between to spread menu items evenly within the available space.
+Logo Changes:
+
+Trace the path of the logo in the Build/Static files and the src/components/Assets to locate the PNG and the SVG Logo picture and name.
+Search for a substitute picture and paste in the exact folders and renaming in the same as the previous logo in order to subtitute the logo design.
+That allows to simpply reload the code and the new logo will be demonstrated.
+The only step is specifying the right file type as PNG for one and SVG for the other.
+logo changes and navbar re-coloured 6. **HomePage Colour changes**: -Changes the colours of the Class card at the Homepage.module.css in order to make the shops pop out more for user view. -Adding lighter colours to the background allows the shop or Card Class to be the main view with its darker colours. homepage colour changes.
+
+Here are the changes made to the CSS to enhance the professional and aesthetic appearance of the navbar:
+
+1. **Navbar Background Color**:
+   - Changed the background color to `#192839` (a dark blue) to give a more professional look.
+   
+2. **Logo Container**:
+   - Set the width to `auto` to dynamically adjust based on the content.
+   - Ensured the elements are evenly spaced and aligned centrally.
+   
+3. **Logo Text**:
+   - Changed the color to `#FFFFFF` (white) for high contrast against the dark background.
+   - Slightly reduced the font size to `1.6em` for better proportion and balance.
+   - Maintained a font weight of `600` for emphasis and a professional appearance.
+   - Applied `text-transform: uppercase` to make the text uppercase for a more formal look.
+   - Added `letter-spacing: 0.05em` for improved readability.
+   - Ensured no default margin is applied by setting `margin: 0`.
+   
+4. **Navigation Menu**:
+   - Set the width to `auto` to dynamically adjust based on the content.
+   - Aligned items centrally vertically for a uniform appearance.
+   - Used `justify-content: space-between` to spread menu items evenly within the available space.
+
+5. **Logo Changes**:
+   - Trace the path of the logo in th build/Static files and the src/components/Assetsto locate the PNG and the SVG Logo picture and name.
+   - Search for a substitute picture of the logo and paste to the specific paths where the logo's are located  and rename it similar to the previous logo in order to replace the logo design.
+   - That allows to simply reload the code and the new logo will be demonstrated.
+   - the only step is specifying the right file type as PNG for one and the SVG for the other.
+   <img width="929" alt="logo changes and navbar re-coloured" src="https://github.com/chikoondoj/QueueManagement-main/assets/170455389/fa05f9df-acc9-452b-889c-58c41bf8c238">
+6. **HomePage Colour Changes**:
+   -Changes to the colour of the class "card" at the Homepage.module.css in order to make the shops pop out for user view.
+   -Adding ligher colours to the backgroung allows the shop or Class "card" to be the main view with its darker colours.
+   <img width="926" alt="homepage colour changes" src="https://github.com/chikoondoj/QueueManagement-main/assets/170455389/91f42120-dc7d-4758-9a4a-cd5a64ffa65e">
+   
+ 7. **validation**
+   - The validation logic in the code might had additional rules that were not met by the provided numbers, we had to  Review the validation code to understand what rules were applied and to  ensure the input meets those rules..
+**before**
+   - ![Screenshot (11)](https://github.com/chikoondoj/QueueManagement-main/assets/171292021/d63d8f7c-17c9-4efa-ab09-7539dab3dff4)
+   - 
+ 
---
+++ src/components/Auth/Login/Login.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import Navbar from '../../Layout/Navbar/Navbar';
+import { useForm } from 'react-hook-form';
+import image from '../../Assets/pic.svg';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+import AuthService from '../../../services/API';
+import { useDispatch } from 'react-redux';
+import { setLoader, UnsetLoader } from '../../../redux/actions/LoaderActions';
+
+const Login = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+        mode: "onTouched"
+    });
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [toggle, setToggle] = useState(false);
+
+    const onSubmit = (data, e) => {
+        dispatch(setLoader());
+        e.preventDefault();
+        let obj = {
+            "email": data.email,
+            "password": data.password,
+            "isStore": data.aopt === "store" ? false : true
+        };
+        AuthService.Login(obj)
+            .then((res) => {
+                dispatch(UnsetLoader());
+                if (res) {
+                    localStorage.setItem("access", res.data.access_token);
+                    localStorage.setItem("refresh", res.data.refresh_token);
+                    localStorage.setItem("userid", res.data._id);
+
+                    !obj.isStore ? navigate("/create-store") : navigate("/");
+                }
+            }).catch((e) => {
+                dispatch(UnsetLoader());
+                setErrorMessage('Please enter the correct email address or password.');
+                console.log(e);
+            });
+    }
+
+    const handleClick = () => {
+        navigate("/forgot");
+    }
+
+    const handleClicked = () => {
+        navigate("/signup");
+    }
+
+    return (
+        <div className='Signup-Page'>
+            <div className='Navbar-Signup'>
+                <Navbar />
+            </div>
+            <div className='middle-portion'>
+                <div className='login-heading'>
+                    <p>Welcome Back Dear customer  <span className='ques'>!</span></p>
+                </div>
+                <form className='input-login' onSubmit={handleSubmit(onSubmit)}>
+                    <div className='radio-button'>
+                        <div className='customer-radio'>
+                            <label className='label-data' htmlFor="field-customer">
+                                <input
+                                    {...register("aopt", { required: "This field is required" })}
+                                    type="radio"
+                                    name="aopt"
+                                    value="customer"
+                                    id="field-customer"
+                                />
+                                customer
+                            </label>
+                        </div>
+                        <div className='store-radio'>
+                            <label className='label-data' htmlFor="field-store">
+                                <input
+                                    {...register("aopt", { required: "This field is required" })}
+                                    type="radio"
+                                    name="aopt"
+                                    value="store"
+                                    id="field-store"
+                                />
+                                Store
+                            </label>
+                        </div>
+                        <p className='alerts'>{errors.aopt?.message}</p>
+                    </div>
+                    <div className='form-container'>
+                        <div className='emails'>
+                            <input className='input-field' type="email" placeholder='Enter Email Address' name="email" {...register("email", { required: "Email is required", pattern: { value: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i, message: "This is not a valid email" } })}></input>
+                            <p className='alerts'>{errors.email?.message}</p>
+                        </div>
+                    </div>
+                    <div className='form-container'>
+                        <div className='passwords'>
+                            <i id="passlock" className="fa fa-eye" aria-hidden="true"></i>
+                            {
+                                toggle ? <i id='passlock' className="fa fa-eye-slash" aria-hidden="true" onClick={() => { setToggle(!toggle) }}></i> : <i id="passlock" className="fa fa-eye" aria-hidden="true" onClick={() => { setToggle(!toggle) }}></i>
+                            }
+                            <input className='input-field' type={toggle ? "text" : "password"} placeholder='Enter Password' name="password" {...register("password", { required: "password is required", minLength: { value: 8, message: "Password must be more than 8 characters" }, maxLength: { value: 14, message: "Password cannot exceed more than 14 characters" } })}></input>
+                            <p className='alerts'>{errors.password?.message}</p>
+                        </div>
+                    </div>
+                    {errorMessage && <p className='alerts'>{errorMessage}</p>}
+                    <p className='forgot' onClick={handleClick}><u>Forgot password ?</u></p>
+                    <button className='signup-btn' type='submit'>Login</button>
+                    <p className='signup-head'>Create New Account <span onClick={handleClicked}>Signup</span></p>
+                </form>
+            </div>
+           
+        </div>
+    );
+}
+
+export default Login;
